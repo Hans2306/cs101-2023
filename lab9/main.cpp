31dba7f938fe79f6b1a2b441d233f72786560140
Hans2306 <117811104+Hans2306@users.noreply.github.com>
2023-05-25 14:40:29 +0800

    Update main.cpp

--- lab9/main.cpp
+++ lab9/main.cpp
@@ -1,40 +1,35 @@
-#include <iostream>
-#include <fstream>
-#include <string>
+#include<iostream>
+#include<fstream>
+#include<string>
 
 using namespace std;
 
-class ReplaceMyString{
-    public:
-    ReplaceMyString(){}
-    void replaceString(string a, string b){
-        ifstream in("main.cpp");
-        ofstream out("rmain.cpp");
-        int i, j, alen = a.length(), blen = b.length();
-        char c;
-        
-        do{
-            in.get(c);
-            if (in.eof()) break;
-            if (c == a[0]){
-                for (i=1; i<a.length(); i++){
-                    in>>c;
-                    if (c != a[i]) break;
-                    if (i == a.length()-1){
-                        for (j=0; j<b.length(); j++)
-                            out << b[j];
-                    }
-                }
-            }else 
-                out<<c;
-        }while(!in.eof());
-        
+class ReplaceMyString {
+private:
+    ifstream in;
+    ofstream out;
+
+public:
+    void replaceString(string a, string b) {
+        in.open("main.cpp");
+        out.open("rmain.cpp");
+
+        string str;
+        while (getline(in, str)) {
+            size_t pos = str.find(a);
+            while (pos != string::npos) {
+                str.replace(pos, a.length(), b);
+                pos = str.find(a, pos + b.length());
+            }
+            out << str << endl;
+        }
+
         in.close();
         out.close();
     }
 };
 
-int main(){
+int main() {
     ReplaceMyString my;
     my.replaceString("IU", "IU is best");
     return 0;
